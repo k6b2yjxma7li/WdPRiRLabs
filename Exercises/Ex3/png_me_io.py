6fe67f060d50592155dcaf5145277738c666fdca
k6b2yjxma7li <rkrasala@gmail.com>
2024-02-05 20:02:14 +0100

    Switched from pool.map to pool.starmap

--- Exercises/Ex3/png_me_io.py
+++ Exercises/Ex3/png_me_io.py
@@ -5,7 +5,8 @@ import numpy
 import re
 import shutil
 # using multiprocessing because python multithreading is still 
-# bound to a single core (python GIL)
+# bound to a single core (python GIL) so it switches between threads instead
+# of running in parallel
 import multiprocessing
 import datetime
 
@@ -22,20 +23,19 @@ png_names = [ahref['href'] for ahref in soup.find_all('a', href=re.compile(".*pn
 # prepare full urls of images
 png_urls = [url+png_name for png_name in png_names]
 
-# Part 1: sequential download
-start_time = datetime.datetime.now()
-for png_url, png_name in zip(png_urls, png_names):
-    resp_png = requests.get(png_url, stream=True)
-    if resp_png.status_code == 200:
-        with open(png_name, "wb") as img:
-            resp_png.raw.decode_content = True
-            shutil.copyfileobj(resp_png.raw, img)
-end_time = datetime.datetime.now()
-print(f"Sequential download of {len(png_names)} png images finished in: {end_time-start_time}")
+# # Part 1: sequential download
+# start_time = datetime.datetime.now()
+# for png_url, png_name in zip(png_urls, png_names):
+#     resp_png = requests.get(png_url, stream=True)
+#     if resp_png.status_code == 200:
+#         with open(png_name, "wb") as img:
+#             resp_png.raw.decode_content = True
+#             shutil.copyfileobj(resp_png.raw, img)
+# end_time = datetime.datetime.now()
+# print(f"Sequential download of {len(png_names)} png images finished in: {end_time-start_time}")
 
 # Part 2: pooled download
-def download_png(args):
-    url, filepath = args
+def download_png(url, filepath, *args):
     try:
         resp_png = requests.get(url, stream=True)
         if resp_png.status_code == 200:
@@ -50,6 +50,7 @@ def download_png(args):
 start_time = datetime.datetime.now()
 pool_size = multiprocessing.cpu_count()
 with multiprocessing.Pool(pool_size) as pool:
-    pool.map(download_png, zip(png_urls, png_names))
+    pool.starmap(download_png, zip(png_urls, png_names))
 end_time = datetime.datetime.now()
 print(f"Process-pooled download of {len(png_names)} png images finished in: {end_time-start_time}")
+
